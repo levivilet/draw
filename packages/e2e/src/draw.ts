716d5c37c0b760e8d03042e56a610346e124f04d
Le Vivilet <levivilet@pm.me>
2026-08-17 18:24:22 +0200

    fix

---
+++ packages/e2e/src/draw.ts
@@ -0,0 +1,10 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'draw.basic'
+
+export const skip = true
+
+export const test: Test = async ({ Command, expect, Locator }) => {
+
+  // TODO
+}
